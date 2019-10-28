cda1f2cc8a2404f69617392d6989a86b4cee0a17
epanks <47796637+epanks@users.noreply.github.com>
2019-10-28 19:38:55 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -25,10 +25,4 @@ Route::post('/create_paket','PaketController@store');
 Route::get('/paket','PaketController@index');
 Route::get('/paket/{id}/edit', 'PaketController@edit');
 Route::post('/paket/{id}/update', 'PaketController@update');
-<<<<<<< HEAD
-Route::get('/paket/{id}', 'PaketController@show');
-=======
-Route::get('/paket/{id}/delete', 'PaketController@delete');
-//Route::get('/paket/{id}/show', 'PaketController@show');
-Route::get('/paketdashboard', 'PaketController@paketdashboard');
->>>>>>> 8d8d16d70001ec797ea027f0bdeb7be336a0263a
+
